85c2c542f8d317a1a8b5653eb5cebaa4e875fd6e
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-07-01 18:11:27 -0400

    Added another loop to the pacman-for-loop.js file

--- assign1/pacman-for-loop.js
+++ assign1/pacman-for-loop.js
@@ -3,3 +3,9 @@ const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
 for (var index = 0; index < ghosts.length; index++) {
     console.log(`Pac-Man eats ${ghosts[index]}.`);
 }
+
+const airplanes = ['Cessna', 'Piper', 'Raytheon', 'Gulfstream'];
+
+for (var index = 0; index < airplanes.length; index++) {
+    console.log('I fly a ' + airplanes[index]);
+}
